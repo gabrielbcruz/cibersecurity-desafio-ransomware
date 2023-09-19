ddda697cdea99713caff09d65e80245fef256fb9
Gabriel da B. Cruz <78567885+gabrielbcruz@users.noreply.github.com>
2023-09-19 19:16:46 -0300

    Update Decrypter.py
    Arquivo de descriptografia

--- Decrypter.py
+++ Decrypter.py
@@ -1,1 +1,22 @@
+import os
+import pyaes
 
+## Deve abrir o arquivo criptografado
+file_name = "teste.txt.ransomwaretroll"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+## Chave de descriptografia
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+decrypt_data = aes.decrypt(file_data)
+
+## Comando para remover o arquivo
+os.remove(file_name)
+
+## Deve se criar o arquivo descriptografado
+new_file = "teste.txt"
+new_file = open(f'{new_file}', "wb")
+new_file.write(decrypt_data)
+new_file.close()
