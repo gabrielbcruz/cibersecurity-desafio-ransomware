59159b496ffea8abb4f68026d05921b87a8851db
Gabriel da B. Cruz <78567885+gabrielbcruz@users.noreply.github.com>
2023-09-19 19:37:53 -0300

    Update Decrypter.py

--- Decrypter.py
+++ Decrypter.py
@@ -1,22 +1,33 @@
 import os
 import pyaes
 
-## Deve abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+def main():
+    # Abrir o arquivo criptografado
+    file_name = "teste.txt.ransomwaretroll"
+    file = open(file_name, "rb")
+    file_data = file.read()
+    file.close()
 
-## Chave de descriptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
+    # Verificar se o arquivo existe
+    if not os.path.exists(file_name):
+        print("O arquivo não existe.")
+        return
 
-## Comando para remover o arquivo
-os.remove(file_name)
+    # Verificar se a chave é válida
+    key = b"testeransomwares"
+    if len(key) != 32:
+        print("A chave deve ter 32 bytes.")
+        return
 
-## Deve se criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+    # Descriptografar os dados
+    aes = pyaes.AESModeOfOperationCTR(key)
+    decrypt_data = aes.decrypt(file_data)
+
+    # Salvar o arquivo descriptografado
+    new_file_name = input("Nome do arquivo descriptografado: ")
+    new_file = open(f'{new_file_name}', "wb")
+    new_file.write(decrypt_data)
+    new_file.close()
+
+if __name__ == "__main__":
+    main()
